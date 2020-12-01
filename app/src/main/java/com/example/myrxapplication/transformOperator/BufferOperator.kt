01f0b6b8e152deaca7cb7d935265e2e9f54b8ac9
praveen p <ppraveen92@gmail.com>
2020-12-01 09:53:34 +0530

    Buffer Operator

---
+++ app/src/main/java/com/example/myrxapplication/transformOperator/BufferOperator.kt
@@ -0,0 +1,42 @@
+package com.example.myrxapplication.transformOperator
+
+import android.util.Log
+import com.example.myrxapplication.MyTAG
+import com.example.myrxapplication.MyTAG.createPersonList
+import com.example.myrxapplication.Person
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+/*
+* Buffer Operator: transform the items in buffered manner
+*
+* */
+fun showBuffer() {
+    val observable: Observable<Person> = Observable
+        .fromIterable(createPersonList())
+        .subscribeOn(
+            Schedulers.io()
+        )
+
+    observable.buffer(2)
+        .subscribe(object : Observer<MutableList<Person>> {
+            override fun onComplete() {
+
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onNext(t: MutableList<Person>) {
+                for (person in t) {
+                    Log.d(MyTAG.mainTag, "onNext : ${person.name}")
+                }
+            }
+
+            override fun onError(e: Throwable) {
+
+            }
+        })
+}
